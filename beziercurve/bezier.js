bd67f699590ad99ca7ce914bfb5747d9f2a881ae
Navneet <navneetmelarkode@gmail.com>
2022-06-21 14:36:18 +0530

    feat: Bezier curve simulation completed

--- beziercurve/bezier.js
+++ beziercurve/bezier.js
@@ -1,27 +1,27 @@
-const canvas = document.getElementById("bezier");
-const ctx = canvas.getContext("2d");
-
+let points = [];
+let controlpoints = [];
+let endpoints = [];
+let addpointbtn;
+let rempointbtn;
+let t_anim = 0;
+let showstringart = false;
+let stringartcheckbox;
 class Point {
-	constructor(x, y) {
+	constructor(x, y, w = 10, h = 10) {
 		this.x = x;
 		this.y = y;
+		this.w = w;
+		this.h = h;
 	}
 }
-
-function plot(p) {
-	ctx.beginPath();
-	ctx.arc(p.x, p.y, 1, 0, Math.PI * 2);
-	ctx.fill();
-}
-function line(p0, p1) {
-	ctx.beginPath();
-	ctx.moveTo(p0.x, p0.y);
-	ctx.lineTo(p1.x, p1.y);
-	ctx.stroke();
-}
-
-let lerp = (t, p1, p2) =>
-	new Point((1 - t) * p1.x + t * p2.x, (1 - t) * p1.y + t * p2.y);
+let lerp = (t, p1, p2) => {
+	return new Point(
+		(1 - t) * p1.x + t * p2.x,
+		(1 - t) * p1.y + t * p2.y,
+		10,
+		10
+	);
+};
 let split = (t, p1, p2, ...ps) => {
 	if (ps.length > 0) {
 		return [lerp(t, p1, p2), ...split(t, p2, ...ps)];
@@ -37,20 +37,87 @@ let deCasteljau = (t, ps) => {
 	}
 };
 
-let randomPoint = () =>
-	new Point(Math.random() * canvas.width, Math.random() * canvas.height);
-let p0 = randomPoint();
-let p1 = randomPoint();
-let p2 = randomPoint();
+let randomPoint = () => {
+	return new Draggable(random(10, width - 10), random(10, height - 10), 10, 10);
+};
+let addPoint = () => {
+	controlpoints.push(randomPoint());
+	points = [endpoints[0], ...controlpoints, endpoints[1]];
+};
+let removePoint = () => {
+	controlpoints.pop();
+	points = [endpoints[0], ...controlpoints, endpoints[1]];
+};
+let checkEvent = () => {
+	if (stringartcheckbox.checked()) {
+		showstringart = true;
+	} else {
+		showstringart = false;
+	}
+};
 
-let points = [p0, p1, p2];
-line(p0, p1);
-line(p1, p2);
+function setup() {
+	createCanvas(640, 360);
+	createP(" ");
+	addpointbtn = createButton("Add Point");
+	rempointbtn = createButton("Remove Point");
+	stringartcheckbox = createCheckbox("Show String Art", false);
+	stringartcheckbox.changed(checkEvent);
+	addpointbtn.mousePressed(addPoint);
+	rempointbtn.mousePressed(removePoint);
+	controlpoints = [randomPoint()];
+	endpoints = [randomPoint(), randomPoint()];
+	points = [endpoints[0], ...controlpoints, endpoints[1]];
+}
+function draw() {
+	background(255);
+	for (let i = 0; i < points.length; i++) {
+		if (i == 0 || i == points.length - 1) {
+			points[i].endpoints = true;
+		}
+		strokeWeight(1);
+		points[i].over();
+		points[i].update();
+		points[i].show();
+	}
+	if (t_anim > 1) {
+		t_anim = 0;
+	}
+	t_anim += 0.005;
+	stroke(0, 255, 0);
+	strokeWeight(10);
+	let p_anim = deCasteljau(t_anim, points);
+	point(p_anim.x, p_anim.y);
+	let prev = points[0];
+	for (let t = 0; t <= 1; t += 0.005) {
+		let p = deCasteljau(t, points);
+		stroke(0);
+		strokeWeight(1);
+		line(prev.x, prev.y, p.x, p.y);
+		prev = p;
+	}
+	if (showstringart) {
+		stroke(0);
+		strokeWeight(1);
+		for (let i = 1; i < points.length; i++) {
+			line(points[i - 1].x, points[i - 1].y, points[i].x, points[i].y);
+			for (let t = 0; t <= 1 && i < points.length - 1; t += 0.05) {
+				let l1 = lerp(t, points[i - 1], points[i]);
+				let l2 = lerp(t, points[i], points[i + 1]);
+				line(l1.x, l1.y, l2.x, l2.y);
+			}
+		}
+	}
+}
+function mousePressed() {
+	for (let i = 0; i < points.length; i++) {
+		points[i].pressed();
+	}
+}
+function mouseReleased() {
+	for (let i = 0; i < points.length; i++) {
+		points[i].released();
+	}
+}
 
-for (var t = 0; t <= 1; t += 0.005) {
-	let p = deCasteljau(t, points);
-	plot(p);
-}
-for (var t = 0; t <= 1; t += 0.05) {
-	line(lerp(t, p0, p1), lerp(t, p1, p2));
-}
+//button click p5
